a6115a80566d0e3b7a916c0eb7205507d07311e2
hosnn <soghtmd@gmail.com>
2024-06-07 15:39:51 +0900

    index페이지 위치정보허용 안해도 처리되도록 수정

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const locContainer = document.querySelector('.location-based');
   
   // getGeoLocation 함수 호출 후 then을 사용하여 connectGeo 및 console.log 호출
   getGeoLocation().then(position => {
@@ -24,42 +23,55 @@ document.addEventListener("DOMContentLoaded", function() {
       lon = 126.9780
     } 
 
-    $.ajax({
-      url: "/test",
-      data: {'lat':lat, 'lon':lon},
-      type: "GET",
-      success: function(response) {
-          console.log(response);
-  
-          response.forEach((data, i) => {
-            const dist = (Math.floor(Number(data.dist))/1000).toFixed(1);
-            let content = `
-              <div class="row" data-aos="fade-up">
-              `;
-            
-            if(i % 2 == 1) {
-              content += `<div class="col-md-5">`;
-            } else {
-              content += `<div class="col-md-5 order-1 order-md-2">`;
-            }
-            content += `
-              <img src="${data.firstimage}" class="img-fluid" alt="">
-                </div>
-                <div class="col-md-7 pt-4">
-                  <h3>${data.title}</h3>
-                  <p>거리 : 약 ${dist}km</p>
-                  <p class="fst-italic">
-                    주소 : ${data.addr1} ${data.addr2}
-                  </p>
-                </div>
-              </div>
-              `;
-            locContainer.innerHTML += content;
-          });
-      },
-      error: function(error) {
-          console.error("Error:", error);
-      }
-    });
-  }).catch(errorGeo);
+    getLocationBasedList(lat, lon);
+    
+  }).catch(() => {
+    // 위치 정보 허용을 안하면 서울 중심으로 위경도 설정
+    lat = 37.5665
+    lon = 126.9780
+
+    getLocationBasedList(lat, lon);
+  });
 });
+
+function getLocationBasedList(lat, lon) {
+  const locContainer = document.querySelector('.location-based');
+  
+  $.ajax({
+    url: "/test",
+    data: {'lat':lat, 'lon':lon},
+    type: "GET",
+    success: function(response) {
+        console.log(response);
+
+        response.forEach((data, i) => {
+          const dist = (Math.floor(Number(data.dist))/1000).toFixed(1);
+          let content = `
+            <div class="row" data-aos="fade-up">
+            `;
+          
+          if(i % 2 == 1) {
+            content += `<div class="col-md-5">`;
+          } else {
+            content += `<div class="col-md-5 order-1 order-md-2">`;
+          }
+          content += `
+            <img src="${data.firstimage}" class="img-fluid" alt="">
+              </div>
+              <div class="col-md-7 pt-4">
+                <h3>${data.title}</h3>
+                <p>거리 : 약 ${dist}km</p>
+                <p class="fst-italic">
+                  주소 : ${data.addr1} ${data.addr2}
+                </p>
+              </div>
+            </div>
+            `;
+          locContainer.innerHTML += content;
+        });
+    },
+    error: function(error) {
+        console.error("Error:", error);
+    }
+  });
+}
